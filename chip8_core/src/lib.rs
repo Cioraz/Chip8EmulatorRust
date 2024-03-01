7e6333a8c9b3b9b371399c48000968bb8a940159
Cioraz <sunilthunga.221cs252@nitk.edu.in>
2024-03-01 10:38:02 +0530

    Emulator Struct

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+const RAM_SIZE: usize = 4096;
+const NUM_REGS: usize = 16;
+const STACK_SIZE: usize= = 16;
+const NUM_KEYS: usize = 16;
+const START_ADDR: u16 = 0x200;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub struct Emulator{
+    pc: hu16,
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_HEIGHT*SCREEN_WIDTH],
+    v: [u8; NUM_REGS],
+    index_reg: u16,
+    stack: [u16; STACK_SIZE],
+    sound_timer: u8,
+    delay_timer: u8,
+    keys: [bool; NUM_KEYS],
 }
