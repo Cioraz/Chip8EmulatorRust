eea8aef0dc55fe16d2915277c4b618c8c213b8e9
Cioraz <sunilthunga.221cs252@nitk.edu.in>
2024-03-01 10:41:35 +0530

    impl for emulator struct

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,4 +1,4 @@
-const RAM_SIZE: usize = 4096;
+const RAM_SIZE: usize = 0x1000; // 4096
 const NUM_REGS: usize = 16;
 const STACK_SIZE: usize= = 16;
 const NUM_KEYS: usize = 16;
@@ -14,7 +14,23 @@ pub struct Emulator{
     v: [u8; NUM_REGS],
     index_reg: u16,
     stack: [u16; STACK_SIZE],
+    stack_ptr: u16,
     sound_timer: u8,
     delay_timer: u8,
     keys: [bool; NUM_KEYS],
+}
+
+impl Emulator{
+    pub fn new() -> Self{
+        pc: START_ADDR,
+        ram: [0; RAM_SIZE],
+        screen: [false; SCREEN_WIDTH*SCREEN_HEIGHT],
+        v: [0; NUM_REGS],
+        index_reg: 0,
+        stack_ptr: 0,
+        stack: [0; STACK_SIZE],
+        keys: [false; NUM_KEYS],
+        delay_timer: 0,
+        sound_timer: 0,
+    }
 }
